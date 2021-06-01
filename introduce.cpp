079b2696ef2222e1fdc2eb73923ce625aa3dcfe6
kartavya kothari <kartavya.kothari24@gmail.com>
2021-06-01 19:32:33 +0530

    Added code

---
+++ introduce.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    if(age>=18){
+        cout<<"Person is an adult";
+    }else{
+        cout<<"Person is not an adult";
+    }
+
+
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 10;
+    // 4 bytes
+    char b = 'd';
+    // 1 bytes
+    float f = 2.45;
+    double d = 23.34;
+
+    char *arr = "Hello";
+
+    // cout<<arr;
+
+    // Only in c++
+    // bool is_happy = true;
+    bool is_happy = false;
+
+    string name = "Kartavya Kothari";
+    cout<<name;
+
+
+    // if(is_happy){
+    //     cout<<"Person is happy";
+    // }else{
+    //     cout<<"Person is unhappy";
+    // }
+    // cout<<sizeof(double);
+
+
+
+
+    return 0;
+}
