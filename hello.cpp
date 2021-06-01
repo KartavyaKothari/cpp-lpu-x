76a187f30def56bc24cfec2580297820dba79300
kartavya kothari <kartavya.kothari24@gmail.com>
2021-06-01 19:14:52 +0530

    Added helloworld

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    // scanf("%d %d",&a,&b);
+    // cin>>a>>b;
+    cin>>a;
+    cin>>b;
+    
+    // printf("Hello world , %d\n",a);
+    cout<<"Hello world "<<a<<" "<<b;
+    // cout<<"Hello world ";
+    // cout<<a;
+    // cout<<" ";
+    // cout<<b;
+
+
+
+
+    return 0;
+}
